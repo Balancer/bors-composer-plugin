5a7aaf2925070bdcc6d5920284cb5f4a8cdbdfbb
Balancer <balancer@balancer.ru>
2015-03-01 05:34:06 +0300

    update

--- src/B2/Composer/Plugin.php
+++ src/B2/Composer/Plugin.php
@@ -30,11 +30,11 @@ class Plugin implements PluginInterface, EventSubscriberInterface
 	{
 		echo "plugin: getSubscribedEvents\n";
 		return array(
-			ScriptEvents::POST_AUTOLOAD_DUMP => array(
+//			ScriptEvents::POST_AUTOLOAD_DUMP => array(
 //			array('onPostAutoloadDump1', 10),
 //				array('onPostAutoloadDump', 0),
 //				array('onPostAutoloadDump3', 30)
-			),
+//			),
 		);
 	}
 
