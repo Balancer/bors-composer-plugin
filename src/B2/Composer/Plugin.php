b45465fa9dbcdcd63e08fbc9d75e85e04e418c2d
Balancer <balancer@balancer.ru>
2015-03-01 05:19:25 +0300

    update

--- src/B2/Composer/Plugin.php
+++ src/B2/Composer/Plugin.php
@@ -5,6 +5,7 @@ namespace B2\Composer;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
+use Composer\EventDispatcher\EventSubscriberInterface;
 
 class Plugin implements PluginInterface
 {
@@ -14,17 +15,44 @@ class Plugin implements PluginInterface
 		return 'bors-component' === $packageType;
 	}
 
-    public static function postAutoloadDump(Event $event)
-    {
-        $composer = $event->getComposer();
-        $io = $event->getIO();
-        $io->write('<info>Test: postAutoloadDump</info>');
+	public function activate(Composer $composer, IOInterface $io)
+	{
+		echo "activate plugin\n";
+//		$installer = new TemplateInstaller($io, $composer);
+//		$composer->getInstallationManager()->addInstaller($installer);
+		$this->composer = $composer;
+		$this->io = $io;
 	}
 
-	public function activate(Composer $composer, IOInterface $io)
-    {
-    	echo "activate plugin\n";
-//        $installer = new TemplateInstaller($io, $composer);
-//        $composer->getInstallationManager()->addInstaller($installer);
-    }
+	public static function getSubscribedEvents()
+	{
+		return array(
+			PluginEvents::POST_AUTOLOAD_DUMP => array(
+				array('onPostAutoloadDump1', 10),
+				array('onPostAutoloadDump', 0),
+				array('onPostAutoloadDump3', 30)
+			),
+		);
+	}
+
+	public static function postAutoloadDump(Event $event)
+	{
+		$composer = $event->getComposer();
+		$io = $event->getIO();
+		$io->write('<info>Test: postAutoloadDump</info>');
+	}
+
+	public static function postAutoloadDump1(Event $event)
+	{
+		$composer = $event->getComposer();
+		$io = $event->getIO();
+		$io->write('<info>Test: postAutoloadDump</info>');
+	}
+
+	public static function postAutoloadDump3(Event $event)
+	{
+		$composer = $event->getComposer();
+		$io = $event->getIO();
+		$io->write('<info>Test: postAutoloadDump</info>');
+	}
 }
