88a2770aabcb7d08fc6d827a178c8fb1784c71c1
Balancer <balancer@balancer.ru>
2015-03-01 05:21:33 +0300

    update

--- src/B2/Composer/Plugin.php
+++ src/B2/Composer/Plugin.php
@@ -3,6 +3,7 @@
 namespace B2\Composer;
 
 use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 use Composer\EventDispatcher\EventSubscriberInterface;
